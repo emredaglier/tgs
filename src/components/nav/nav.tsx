86e244129d62cf58a5fa2aae48c7c5947b7cbf33
Emre Daglier <emredaglier@gmail.com>
2024-09-16 11:44:11 +0300

    dropdown fix

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -11,7 +11,7 @@ const Nav = () => {
   return (
     <>
       <div
-        className={`fixed w-screen z-50 overflow-hidden bg-white/90 backdrop-blur-md dark:bg-[--background] border-b dark:border-b-gray-800 text-[--foreground] ${margin} py-[20px] `}
+        className={`fixed w-screen z-50 bg-white/90 backdrop-blur-md dark:bg-[--background] border-b dark:border-b-gray-800 text-[--foreground] ${margin} py-[20px] `}
       >
         <header className="flex flex-row items-center justify-between">
           <NavLogo />
